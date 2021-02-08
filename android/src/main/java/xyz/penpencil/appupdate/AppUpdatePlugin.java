567ab0d2ddbd5bddcd6db2098368241b0866d7bd
Suho Lee <susemeee@gmail.com>
2021-02-08 16:05:52 +0900

    Update AppUpdatePlugin.java

--- android/src/main/java/xyz/penpencil/appupdate/AppUpdatePlugin.java
+++ android/src/main/java/xyz/penpencil/appupdate/AppUpdatePlugin.java
@@ -24,6 +24,7 @@ import org.apache.commons.io.FileUtils;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
