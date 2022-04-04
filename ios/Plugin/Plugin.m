33f8a4c05f1cd53da883089c89c6f3a3a8433a3f
Seungho Hong <seunghonice@gmail.com>
2022-04-04 12:41:50 +0900

    feat: CAP_PLUGIN_METHOD(checkUpdatePath) 추가

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,6 +5,7 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(AppUpdatePlugin, "AppUpdatePlugin",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(checkUpdatePath, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(getAppInfo, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(updatePref, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(downloadUpdate, CAPPluginReturnPromise);
